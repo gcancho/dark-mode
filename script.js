11d2569e158b00ac930810952ca137317c27cf29
gcancho <giovannicb19@gmail.com>
2020-11-12 12:33:48 -0500

    Texto del boton cambia exitosamente

--- script.js
+++ script.js
@@ -4,12 +4,32 @@ const pagina = document.querySelector('body');
 const configUser = window.matchMedia('(prefers-color-scheme: dark)');
 //LocalStorage, llave : tema
 const localConfig = localStorage.getItem('tema');
+const textoConfig = localStorage.getItem('texto');
+
+let textoBoton;let contenidoBoton;
 
 if(localConfig === 'dark'){
     pagina.classList.toggle('dark-mode');
+    textoBoton = 'oscuro';
 }else if(localConfig === 'light'){
     pagina.classList.toggle('light-mode');
+    textoBoton = 'blanco';
+}else{
+    if(configUser.matches){
+        textoBoton = 'oscuro';
+    }else{
+        textoBoton = 'blanco';
+    }
 }
+
+if(textoBoton === 'oscuro')
+boton.innerHTML = 'Ir a modo claro';
+else{
+boton.innerHTML = 'Ir a modo oscuro';
+}
+
+localStorage.setItem('texto', textoBoton);
+
 
 boton.addEventListener('click',()=>{
     //Creando variable tipo let porque se va a reescribir
@@ -33,7 +53,21 @@ boton.addEventListener('click',()=>{
         }
     }
 
-  
-
+    if(colorTema === 'dark'){
+        textoBoton = 'oscuro';
+    }else{
+        textoBoton = 'blanco';
+    }
+    
     localStorage.setItem('tema', colorTema);
+    localStorage.setItem('texto', textoBoton);
+    
+    
+    if(textoBoton === 'oscuro')
+        boton.innerHTML = 'Ir a modo claro';
+    else{
+        boton.innerHTML = 'Ir a modo oscuro';
+    }
+    
+                
 });
